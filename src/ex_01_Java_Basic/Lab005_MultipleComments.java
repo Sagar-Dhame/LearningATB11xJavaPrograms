78a98da1528232f2f045d77f6d378a5c84536498
Sagar Dhame <sagarsureshdhame@gmail.com>
2025-03-10 23:47:44 +0530

    10 March Exercise

---
+++ src/ex_01_Java_Basic/Lab005_MultipleComments.java
@@ -0,0 +1,9 @@
+//Author - Sagar
+package ex_01_Java_Basic;
+//Program details - Hello World
+public class Lab005_MultipleComments {
+    public static void main(String[] args) {
+//        This is also a comment
+        System.out.println("Hello");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab006_Comments_Multiline.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab006_Comments_Multiline {
+    public static void main(String[] args) {
+        System.out.println("Multiline comment");
+        /**
+        * Author : Sagar Dhame
+        * Course :  Java
+        * Topic :  Multi line comment
+        **/
+    }
+}
