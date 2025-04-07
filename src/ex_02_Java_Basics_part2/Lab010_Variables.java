2565c483309f989a7ee097e16f720e719723dbef
Sagar Dhame <sagarsureshdhame@gmail.com>
2025-04-07 23:45:11 +0530

    07 April

---
+++ src/ex_02_Java_Basics_part2/Lab010_Variables.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics_part2;
+
+public class Lab010_Variables {
+    public static void main(String[] args) {
+        //int _=65;
+        int $=66;
+        int this_is_a_vari_long_name_in_the_class =67;
+        System.out.println($);
+    }
+}
---
+++ src/ex_02_Java_Basics_part2/Lab011_InterviewQnA.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics_part2;
+
+public class Lab011_InterviewQnA {
+    public static void main(String[] args) {
+        int _2= 123;
+        System.out.println(_2);
+        // We cannot use only underscore
+    }
+}
